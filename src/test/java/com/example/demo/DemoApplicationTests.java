4d75415aab037b12343701ef7eea124a9e72b12e
Siryeong <SiryeongChoi@gmail.com>
2023-02-16 00:01:01 +0900

    test: abstract test 클래스 선언

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,15 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class DemoApplicationTests {
 	Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 
-	@Test
-	void contextLoads() {
-		logger.info("hello world");
-	}
+	@LocalServerPort
+	private int port;
 
 }
