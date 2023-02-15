85a208b963a9cb53c584575b7459d489ce6865b5
Siryeong <SiryeongChoi@gmail.com>
2023-02-15 13:36:16 +0900

    test: hello world

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("hello world");
 	}
 
 }
